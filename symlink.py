24211c194bbdcd8ee8b854ea642321d5abcf1544
Vitor Hugo <vtr@hotmail.com.br>
2016-04-10 15:38:22 -0300

    Print Header function

--- symlink.py
+++ symlink.py
@@ -4,8 +4,14 @@ import os
 import argparse
 import sys
 
+VIDEO_EXT = ['.mkv', '.mp4']
 
-def create_symlinks(origin, dest):
+
+def list_files(path):
+    return [file for path, directories, files in os.walk(path) for file in files]
+
+
+def create_symlinks(origin, dest, files):
     folder_name = os.path.split(origin)[1]
     folder_destination = os.path.join(dest, folder_name)
     for path, directories, files in os.walk(origin):
@@ -14,10 +20,35 @@ def create_symlinks(origin, dest):
             file_origin = os.path.join(origin, file)
             if os.path.exists(folder_destination):
                 if not os.path.exists(file_destination):
-                    os.symlink(file_origin, file_destination)
+                    try:
+                        print('Folder already exists, just creating symlinks')
+                        os.symlink(file_origin, file_destination)
+                        print("File: {} symlinked to {}\n".format(
+                            file_origin, file_destination))
+                    except FileExistsError:
+                        print(
+                            "File {} already exists. Skipping it".format(file_destination))
+                        pass
             else:
                 os.makedirs(folder_destination)
+                print("Creating folder {}".format(folder_destination))
                 os.symlink(file_origin, file_destination)
+                print("File: {} symlinked to {}\n".format(
+                    file_origin, file_destination))
+
+
+def print_header(file_list, origin, destination):
+    print('There are {} files. These are the paths:'.format(len(file_list)))
+    no_video_files = []
+    for file in file_list:
+        if os.path.splitext(file)[1] in VIDEO_EXT:
+            print('{} -> {}'.format('{}\{}'.format(origin, file),
+                                    '{}\{}\n'.format(destination, file)))
+        else:
+            no_video_files.append(file)
+    if no_video_files:
+        for file in no_video_files:
+            print(file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -26,10 +57,19 @@ if __name__ == '__main__':
         '--origin', '-o', help='absolute origin path', action='store', required=True)
     parser.add_argument(
         '--destination', '-d', help='absolute destination path', action='store', required=True)
+    parser.add_argument(
+        '--silent', '-s', help='to not print information about the process', action='store_true', default=False)
     args = parser.parse_args()
-    confirmation = input(
-        'Origin: {}\nDestination: {}\nAre these paths correct? (y/n) '.format(args.origin, args.destination))
+
+    file_list = list_files(args.origin)
+    # Printing information
+    if not args.silent:
+        print_header(file_list, args.origin, args.destination)
+
+    confirmation = input("Is this information correct? ")
+
     if confirmation.lower() == 'y':
         create_symlinks(args.origin, args.destination)
     else:
+        print("Exiting...")
         sys.exit()
