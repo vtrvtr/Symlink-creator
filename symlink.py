b72878816350888f2deccc8a3e28675a47dc6e82
Vitor Hugo <vtr@hotmail.com.br>
2016-05-14 09:19:52 -0300

    implemented hardlink functionality

--- symlink.py
+++ symlink.py
@@ -3,6 +3,7 @@
 import os
 import argparse
 import sys
+from ntfsutils import hardlink
 
 VIDEO_EXT = ['.mkv', '.mp4']
 
@@ -11,7 +12,7 @@ def list_files(path):
     return [file for path, directories, files in os.walk(path) for file in files]
 
 
-def create_symlinks(origin, dest, files):
+def create_link(origin, dest, files, mode='symlink'):
     folder_name = os.path.split(origin)[1]
     folder_destination = os.path.join(dest, folder_name)
     for path, directories, files in os.walk(origin):
@@ -21,21 +22,26 @@ def create_symlinks(origin, dest, files):
             if os.path.exists(folder_destination):
                 if not os.path.exists(file_destination):
                     try:
-                        print('Folder already exists, just creating symlinks')
-                        os.symlink(file_origin, file_destination)
-                        print("File: {} symlinked to {}\n".format(
-                            file_origin, file_destination))
+                        print('Folder already exists, just creating {}\n'.format(mode))
+                        if mode == 'symlink':
+                            os.symlink(file_origin, file_destination)
+                        else:
+                            hardlink.create(file_origin, file_destination)
+                        print("File: {} linked to {}\n ({})\n".format(
+                            file_origin, file_destination, mode))
                     except FileExistsError:
                         print(
-                            "File {} already exists. Skipping it".format(file_destination))
+                            "File {} already exists. Skipping it\n".format(file_destination))
                         pass
             else:
                 os.makedirs(folder_destination)
                 print("Creating folder {}".format(folder_destination))
-                os.symlink(file_origin, file_destination)
-                print("File: {} symlinked to {}\n".format(
-                    file_origin, file_destination))
-
+                if mode == 'symlink':
+                    os.symlink(file_origin, file_destination)
+                else:
+                    hardlink.create(file_origin, file_destination)
+                print("File: {} symlinked to {}({})\n".format(
+                    file_origin, file_destination, mode))
 
 def print_header(file_list, origin, destination):
     print('There are {} files. These are the paths:'.format(len(file_list)))
@@ -56,7 +62,9 @@ if __name__ == '__main__':
     parser.add_argument(
         '--origin', '-o', help='absolute origin path', action='store', required=True)
     parser.add_argument(
-        '--destination', '-d', help='absolute destination path', action='store', required=True)
+        '--destination', '-d', help='absolute destination path', action='store', required=True),
+    parser.add_argument(
+        '--mode', '-m', help='specify the mode to create the link: hardlink (h) or symlink (s)', action='store', required=False, default='s')
     parser.add_argument(
         '--silent', '-s', help='to not print information about the process', action='store_true', default=False)
     args = parser.parse_args()
@@ -65,11 +73,13 @@ if __name__ == '__main__':
     # Printing information
     if not args.silent:
         print_header(file_list, args.origin, args.destination)
-
     confirmation = input("Is this information correct? ")
 
     if confirmation.lower() == 'y':
-        create_symlinks(args.origin, args.destination, file_list)
+        if args.mode[0].lower() == 's': 
+            create_link(args.origin, args.destination, file_list, mode='symlink')
+        elif args.mode[0].lower() == 'h':
+            create_link(args.origin, args.destination, file_list, mode='hardlink')
     else:
         print("Exiting...")
         sys.exit()
